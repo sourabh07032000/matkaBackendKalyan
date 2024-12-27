30c34ac80ce5d8d7d8f4f4651d96f3b6c6425fb1
sourabh choudhary <113441171+sourabh07032000@users.noreply.github.com>
2024-12-28 01:48:20 +0530

    Create Bet.js

---
+++ models/Bet.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const betSchema = new mongoose.Schema({
+  betAmount: {
+    type: Number,
+    required: true,
+  },
+  matkaBetType: {
+    type: Object,
+    required: true,
+  },
+  matkaBetNumber: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId, // Reference to a User model
+    ref: 'User',
+    required: true,
+  },
+  betPlacedTiming: {
+    type: Date,
+    default: Date.now,
+  },
+  market_id: {
+    type: String,
+    required: true,
+  },
+  betTime: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Bet', betSchema);
