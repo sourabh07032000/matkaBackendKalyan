cf4298dc189699e762f4aecbba9732add9d53eb2
sourabh choudhary <113441171+sourabh07032000@users.noreply.github.com>
2025-01-06 11:57:59 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,12 +2,26 @@ const mongoose = require('mongoose');
 
 // Define the schema for signup including investment details
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  mobileNumber: { type: String, required: true, unique: true },
-  password: { type: String, required: true},
-  mPin: { type: String, required: true },
-  transactionRequest : Array,
-  betDetails : Array,
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  mobileNumber: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true
+  },
+  mPin: { 
+    type: String, 
+    required: true 
+  },
+  transactionRequest: Array,
+  betDetails: Array,
   bankDetails: {
     accountNumber: String,
     ifscCode: String,
@@ -16,9 +30,13 @@ const UserSchema = new mongoose.Schema({
     isApproved: {
       type: Boolean,
       default: false
-    },
-  withdrawalRequest : Array,
-  wallet: {type: Number, default: 200}
+    }
+  }, // Added missing closing brace here
+  withdrawalRequest: Array,
+  wallet: {
+    type: Number, 
+    default: 200
+  }
 }, {
   timestamps: true
 });
