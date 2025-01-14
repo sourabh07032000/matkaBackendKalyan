9d55723cc85f149da81b735a374153e2939b7139
sourabh choudhary <113441171+sourabh07032000@users.noreply.github.com>
2025-01-14 17:00:31 +0530

    Update notificationService.js

--- services/notificationService.js
+++ services/notificationService.js
@@ -1,1 +1,23 @@
+const admin = require('firebase-admin');
+
+// Function to send a notification
+const sendMarketUpdateNotification = async (token, title, body) => {
+  const message = {
+    notification: {
+      title,
+      body,
+    },
+    token,
+  };
+
+  try {
+    const response = await admin.messaging().send(message);
+    console.log('Notification sent successfully:', response);
+  } catch (error) {
+    console.error('Error sending notification:', error);
+    throw error;
+  }
+};
+
+module.exports = { sendMarketUpdateNotification };
 
