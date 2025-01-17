79828562f0898390b18bc5607f4e1700ee36b7c1
sourabh choudhary <113441171+sourabh07032000@users.noreply.github.com>
2025-01-17 17:22:08 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const mongoose = require('mongoose');
+const SlabRate = require("../models/SlabRate");
 
 
 // POST: User route (Create)
