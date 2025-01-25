204b91d4261c826a024b0cddee0a9ecf511f21ac
sourabh choudhary <113441171+sourabh07032000@users.noreply.github.com>
2025-01-25 20:11:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const jsonServer = require('json-server');
 
 const app = express();
 
