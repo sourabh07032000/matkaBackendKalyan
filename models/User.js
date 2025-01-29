e0e21b9c3037e8fd30507dee6e10c21e3c75e607
sourabh choudhary <113441171+sourabh07032000@users.noreply.github.com>
2025-01-29 22:01:28 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,7 +5,9 @@ const UserSchema = new mongoose.Schema({
   username: { 
     type: String, 
     required: true, 
-    unique: true 
+    unique: true,
+    match: [/^[A-Za-z]+$/, "Username can only contain alphabetic characters"]
+
   },
   mobileNumber: { 
     type: String, 
