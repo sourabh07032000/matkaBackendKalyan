9084d7da4f4387c543c6d5e96566db04da0d5c91
sourabh choudhary <113441171+sourabh07032000@users.noreply.github.com>
2025-02-26 11:12:18 +0530

    Update betRoutes.js

--- routes/betRoutes.js
+++ routes/betRoutes.js
@@ -17,7 +17,7 @@ router.post("/place-bet", async (req, res) => {
         if (!market) return res.status(404).json({ message: "Market not found" });
 
        // Convert bet closing time (e.g., "11:58 PM") to a Date object with today's date
-        const betClosingTime = moment(market.betCloseTime, "hh:mm A").toDate();
+        const betClosingTime = moment(market.close_time_formatted, "hh:mm A").toDate();
         const currentTime = new Date();
 
         // Check if the bet close time has passed
