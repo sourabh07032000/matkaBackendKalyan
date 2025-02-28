31dd136efdc9d926e9cd24d1d6825ce7161e1a66
sourabh choudhary <113441171+sourabh07032000@users.noreply.github.com>
2025-02-28 13:21:24 +0530

    Update betRoutes.js

--- routes/betRoutes.js
+++ routes/betRoutes.js
@@ -21,13 +21,13 @@ router.post("/place-bet", async (req, res) => {
         const currentTime = moment().tz("Asia/Kolkata").toDate();
         
 
-        console.log("Condition: ", currentTime >= betOpeningTime && market.aankdo_open == "", currentTime, betOpeningTime,  market.aankdo_open, )
+        console.log("Condition: ", currentTime >= betOpeningTime && market.aankdo_open == "XXX", currentTime, betOpeningTime,  market.aankdo_open, )
         // Check if the bet close time has passed
         if (currentTime >= betClosingTime) {
             return res.status(400).json({ message: "Bet placement closed for this market." });
         }
 
-        if (currentTime >= betOpeningTime && market.aankdo_open == "") {
+        if (currentTime >= betOpeningTime && market.aankdo_open == "XXX") {
             return res.status(400).json({ message: "Bet placement closed for this market." });
         }
 
