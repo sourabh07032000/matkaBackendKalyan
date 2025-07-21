3c5aa1afe64dfeb97e3359aeb24239c9c38bd7bb
sourabh choudhary <113441171+sourabh07032000@users.noreply.github.com>
2025-07-21 15:26:51 +0530

    Update firebase-config.js

--- routes/firebase-config.js
+++ routes/firebase-config.js
@@ -1,6 +1,6 @@
 // firebase-config.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-key.json'); // Your Firebase Admin SDK key
+const serviceAccount = require('../serviceAccountKey.json'); // Your Firebase Admin SDK key
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
